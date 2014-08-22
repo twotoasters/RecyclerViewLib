7b01fb3efbdd63b6d87c83764c2b1f1604bfb9b4
Chris Pierick <chris@twotoasters.com>
2014-08-22 13:40:55 -0400

    Added library project and refactored file structure.

---
+++ library/src/main/java/com/twotoasters/utils/DisplayHelper.java
@@ -0,0 +1,18 @@
+package com.twotoasters.utils;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.view.Display;
+import android.view.WindowManager;
+
+public final class DisplayHelper {
+    private DisplayHelper() { }
+
+    public static Point getScreenDimensions(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+        return point;
+    }
+}
---
+++ sample/src/main/java/com/twotoasters/recycled/factory/ItemAnimationFactory.java
@@ -0,0 +1,30 @@
+package com.twotoasters.recycled.factory;
+
+import android.support.v7.widget.DefaultItemAnimator;
+import com.twotoasters.anim.PendingItemAnimator;
+import com.twotoasters.recycled.animator.FlipDownItemAnimator;
+import com.twotoasters.recycled.animator.FlyItemAnimator;
+import com.twotoasters.recycled.animator.FromTopItemAnimator;
+import com.twotoasters.recycled.animator.GarageDoorItemAnimator;
+
+public final class ItemAnimationFactory {
+    public static final Class<? extends PendingItemAnimator>[] ANIMATOR_ARRAY = new Class[] { DefaultItemAnimator.class
+            , FlipDownItemAnimator.class, FlyItemAnimator.class, FromTopItemAnimator.class, GarageDoorItemAnimator.class };
+
+    private ItemAnimationFactory() { }
+
+    public static PendingItemAnimator getAnimator(int index) {
+        return getAnimator(ANIMATOR_ARRAY[index]);
+    }
+
+    public static <T extends PendingItemAnimator>  T getAnimator(Class<T> animatorClass) {
+        try {
+            return animatorClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
